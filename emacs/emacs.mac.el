0ab1a9095609eb76b2355b42bae38d0b102ccc6d
Sanjin Sehic <saserr@gmail.com>
2015-10-19 16:02:48 +0100

    Fix emacs configuration to properly check if in graphical mode

--- emacs/emacs.mac.el
+++ emacs/emacs.mac.el
@@ -2,11 +2,11 @@
 (setq mac-command-modifier 'meta)
 
 ;; make delete key delete forward on Mac OS X
-(if (and (equal system-type 'darwin) window-system)
+(if (and (equal system-type 'darwin) (display-graphic-p))
     (normal-erase-is-backspace-mode 1))
 
 ;; use same path as login shell on Mac OS X
-(if (and (equal system-type 'darwin) window-system)
+(if (and (equal system-type 'darwin) (display-graphic-p))
     (let ((path-from-shell 
 	   (replace-regexp-in-string "[[:space:]\n]*$" "" 
 				     (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
