5eddf4bf32c5b08cb1e10cbcd03e7da5b03568de
Sanjin Sehic <saserr@gmail.com>
2015-10-19 14:50:36 +0100

    Setup of login shell

--- mac/zsh.sh
+++ mac/zsh.sh
@@ -8,6 +8,24 @@ case $answer in
   Yes)
     echo "[homebrew] installing zsh ..."
     brew install zsh
+    if [ $SHELL != /usr/local/bin/zsh ]
+    then
+      echo "[homebrew] do you want to set this /usr/local/bin/zsh as login shell (current: $SHELL) (Yes / No)? "
+      answer=$(yes_or_no)
+      case $answer in
+        Yes)
+          echo "[homebrew] changing login shell ..."
+          if ! grep -q /usr/local/bin/zsh /etc/shells
+          then
+            sudo echo /usr/local/bin/zsh >> /etc/shells
+          fi
+          chsh -s /usr/local/bin/zsh
+          ;;
+        No)
+          echo "[homebrew] $SHELL will remain the login shell"
+          ;;
+      esac
+    fi
     ;;
   No)
     echo "[homebrew] zsh will not be installed"
