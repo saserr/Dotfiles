54c53ea9a920f71323ef5543b6ec6c6cb6cb712f
Sanjin Sehic <saserr@gmail.com>
2023-02-20 21:44:50 +0000

    Don't use 'declare -n' on bash versions < 4.3

--- lib/variable/nonempty.bash
+++ lib/variable/nonempty.bash
@@ -12,10 +12,21 @@ variable::nonempty() {
   fi
 
   if variable::is_array "$name"; then
-    declare -n array="$name"
-    ((${#array[@]}))
+    # 'declare -n' requires bash version >= 4.3
+    if (("${BASH_VERSINFO[0]}" > 4)) \
+      || ((("${BASH_VERSINFO[0]}" == 4) && ("${BASH_VERSINFO[1]}" >= 3))); then
+      # check if array has size > 0
+      declare -n array="$name"
+      ((${#array[@]}))
+    elif (("${BASH_VERSINFO[0]}" < 4)); then
+      # check if first element of the array is not null. note that this check
+      # does not work with an associative array, hence it is not safe to use it
+      # on bash versions >= 4.
+      [[ -n "${!name[0]+declared}" ]]
+    else # bash versions between [4, 4.2] (untested)
+      eval "((\${#${name}[@]}))"
+    fi
   else
-    declare -n variable="$name"
-    [[ "$variable" ]]
+    [[ "${!name}" ]]
   fi
 }
