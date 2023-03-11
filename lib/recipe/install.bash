c857cf088935959c69e2c94cc8cb1a4d9802fc6a
Sanjin Sehic <saserr@gmail.com>
2023-03-11 08:57:51 +0000

    Add missing arguments::expect in recipe::install

--- lib/recipe/install.bash
+++ lib/recipe/install.bash
@@ -2,16 +2,22 @@ import 'platform::name'
 
 case "$(platform::name)" in
   'mac')
+    import 'arguments::expect'
     import 'mac::install'
 
     recipe::install() {
+      arguments::expect $# # none
+
       mac::install
     }
     ;;
   'debian')
+    import 'arguments::expect'
     import 'debian::install'
 
     recipe::install() {
+      arguments::expect $# # none
+
       debian::install
     }
     ;;
