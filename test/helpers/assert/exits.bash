42821a6031e15370bc92c869bcc71e9e1515a99f
Sanjin Sehic <saserr@gmail.com>
2023-04-12 21:53:21 +0000

    Use arguments in the exit test in assert::exits

--- test/helpers/assert/exits.bash
+++ test/helpers/assert/exits.bash
@@ -3,6 +3,21 @@ import 'arguments::expect'
 import 'path::exists'
 import 'temporary::file'
 
+__assert::exits::test() {
+  arguments::expect $# 'unexpected' 'command' '[argument]' '...'
+
+  local unexpected=$1
+  local command=$2
+  local arguments=("${@:3}")
+
+  $command "${arguments[@]}"
+
+  # this code should be unreachable, because $command is expected to exit
+  local -i status=$?
+  touch "$unexpected"
+  return "$status"
+}
+
 assert::exits() {
   arguments::expect $# 'command' '[argument]' '...'
 
@@ -14,17 +29,7 @@ assert::exits() {
   rm "$unexpected"                         # delete the temporary file
   assert::fails path::exists "$unexpected" # check that it doesn't exist
 
-  # shellcheck disable=SC2317
-  # __test is called by the run method bellow
-  __test() {
-    $command "${arguments[@]}"
-
-    # this code should be unreachable, because $command is expected to exit
-    local -i status=$?
-    touch "$unexpected"
-    return "$status"
-  }
-  run __test
+  run __assert::exits::test "$unexpected" "$command" "${arguments[@]}"
 
   # check that the temporary file still doesn't exist
   assert::fails path::exists "$unexpected"
